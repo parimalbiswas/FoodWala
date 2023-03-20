688342f8f049ca6715495606dc889f4ade37947b
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-03-20 22:40:27 +0530

    .

---
+++ FoodWala/src/main/java/com/foodwala/FoodWalaApplication.java
@@ -0,0 +1,13 @@
+package com.foodwala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodWalaApplication.class, args);
+	}
+
+}
---
+++ FoodWala/src/main/java/com/foodwala/model/Item.java
@@ -0,0 +1,5 @@
+package com.foodwala.model;
+
+public class Item {
+
+}
