d4beb4ca1ec0030a773662622e7eea899013fabb
pawanyadav96 <zib494@gmail.com>
2023-03-21 12:49:27 +0530

    model

--- FoodWala/src/main/java/com/foodwala/model/Customer.java
+++ FoodWala/src/main/java/com/foodwala/model/Customer.java
@@ -1,5 +1,42 @@
 package com.foodwala.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String mobileNumber;
+	
+	
+	private String email;
+	
+	
+	private String password;
+	
+	
+	 @OneToOne(cascade = CascadeType.ALL)
+     private Address address;
+	
 
 }
