1f4981b31606303c844620864aaecef638350d48
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-03-22 17:41:31 +0530

    .

--- FoodWala/src/main/java/com/foodwala/Service/CustomerService.java
+++ FoodWala/src/main/java/com/foodwala/Service/CustomerService.java
@@ -1,14 +1,14 @@
-package com.foodwala.Service;
+package com.foodwala.service;
 
 import com.foodwala.exception.CustomerException;
 import com.foodwala.model.Customer;
 
-public interface CustomerService {
-	
+public interface CustomerService
+{
 
 	public Customer addCustomer(Customer customer) throws CustomerException;
-	
-	public Customer updateCustomer(Integer customerId,Customer customer) throws CustomerException;
-	
+
+	public Customer updateCustomer(Integer customerId, Customer customer) throws CustomerException;
+
 	public Customer deleteCustomer(Integer customerId) throws CustomerException;
 }
