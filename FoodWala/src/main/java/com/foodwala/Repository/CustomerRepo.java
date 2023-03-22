88f12fc87f2e2ec87c6d4491c6555331d3e39394
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-03-22 17:01:24 +0530

    package updated capital to small

--- FoodWala/src/main/java/com/foodwala/Repository/CustomerRepo.java
+++ FoodWala/src/main/java/com/foodwala/Repository/CustomerRepo.java
@@ -1,11 +1,12 @@
-package com.foodwala.Repository;
+package com.foodwala.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.foodwala.model.Customer;
 
-public interface CustomerRepo extends JpaRepository<Customer, Integer> {
-	
+public interface CustomerRepo extends JpaRepository<Customer, Integer>
+{
+
 	public Customer findByMobileNumber(String mobileNumber);
 
 }
--- FoodWala/src/main/java/com/foodwala/Service/CustomerServiceImpl.java
+++ FoodWala/src/main/java/com/foodwala/Service/CustomerServiceImpl.java
@@ -1,92 +1,92 @@
-package com.foodwala.Service;
+package com.foodwala.service;
 
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.foodwala.Repository.CustomerRepo;
 import com.foodwala.exception.CustomerException;
 import com.foodwala.model.Address;
 import com.foodwala.model.Customer;
+import com.foodwala.repository.CustomerRepo;
 
 @Service
-public class CustomerServiceImpl implements CustomerService{
+public class CustomerServiceImpl implements CustomerService
+{
 
 	@Autowired
 	public CustomerRepo cRepo;
-	
-	
+
 	@Override
-	public Customer addCustomer(Customer customer) throws CustomerException {
-		Customer c=cRepo.findByMobileNumber(customer.getMobileNumber());
-		if(c==null)
+	public Customer addCustomer(Customer customer) throws CustomerException
+	{
+		Customer c = cRepo.findByMobileNumber(customer.getMobileNumber());
+		if (c == null)
 		{
 			Customer newCustomer = cRepo.save(customer);
 			return newCustomer;
-		} else {
+		}
+		else
+		{
 			throw new CustomerException("You are already registered");
 		}
-		
+
 	}
 
-
 	@Override
-	public Customer updateCustomer(Integer customerId, Customer customer) throws CustomerException {
-	Optional<Customer> cust=cRepo.findById(customerId);
-	
-	if(cust.isPresent())
+	public Customer updateCustomer(Integer customerId, Customer customer) throws CustomerException
 	{
-		Customer updated=cust.get();
-		
-		updated.setFirstName(customer.getFirstName());
-		updated.setLastName(customer.getLastName());
-		updated.setEmail(customer.getEmail());
-		updated.setMobileNumber(customer.getMobileNumber());
-		updated.setPassword(customer.getMobileNumber());
-		
-		Address add=new Address();
-		
-		add.setBuildingName(customer.getAddress().getBuildingName());
-		add.setArea(customer.getAddress().getArea());
-		add.setCity(customer.getAddress().getCity());
-		add.setPincode(customer.getAddress().getPincode());
-		add.setStreetNo(customer.getAddress().getStreetNo());
-		add.setState(customer.getAddress().getState());
-		
-		updated.setAddress(add);
-		
-		cRepo.save(updated);
-		return updated;
-		
-		}
-	else {
-		throw new CustomerException("Nothing found with this id");
-	}
-	}
+		Optional<Customer> cust = cRepo.findById(customerId);
 
-
-	@Override
-	public Customer deleteCustomer(Integer customerId) throws CustomerException {
-		Optional<Customer> cust=cRepo.findById(customerId);
-		
-		if(cust.isPresent())
+		if (cust.isPresent())
 		{
-			Customer deletedcustomer=cust.get();
-			
-			cRepo.delete(deletedcustomer);
-			
-			return deletedcustomer;
-			
+			Customer updated = cust.get();
+
+			updated.setFirstName(customer.getFirstName());
+			updated.setLastName(customer.getLastName());
+			updated.setEmail(customer.getEmail());
+			updated.setMobileNumber(customer.getMobileNumber());
+			updated.setPassword(customer.getMobileNumber());
+
+			Address add = new Address();
+
+			add.setBuildingName(customer.getAddress().getBuildingName());
+			add.setArea(customer.getAddress().getArea());
+			add.setCity(customer.getAddress().getCity());
+			add.setPincode(customer.getAddress().getPincode());
+			add.setStreetNo(customer.getAddress().getStreetNo());
+			add.setState(customer.getAddress().getState());
+
+			updated.setAddress(add);
+
+			cRepo.save(updated);
+			return updated;
+
 		}
-		else {
+		else
+		{
 			throw new CustomerException("Nothing found with this id");
 		}
-		
+	}
+
+	@Override
+	public Customer deleteCustomer(Integer customerId) throws CustomerException
+	{
+		Optional<Customer> cust = cRepo.findById(customerId);
+
+		if (cust.isPresent())
+		{
+			Customer deletedcustomer = cust.get();
+
+			cRepo.delete(deletedcustomer);
+
+			return deletedcustomer;
+
+		}
+		else
+		{
+			throw new CustomerException("Nothing found with this id");
+		}
+
 	}
 }
-	
-	
-	
-
-
