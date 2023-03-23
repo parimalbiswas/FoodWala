307283b059bd6dc2385be54a80ff7e271ff91946
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-03-23 19:25:55 +0530

    .

--- FoodWala/src/main/java/com/foodwala/controller/RestaurantController.java
+++ FoodWala/src/main/java/com/foodwala/controller/RestaurantController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.foodwala.exception.RestaurantException;
@@ -16,6 +17,7 @@ import com.foodwala.model.Restaurant;
 import com.foodwala.service.RestaurantService;
 
 @RestController
+@RequestMapping("/restaurant")
 public class RestaurantController
 {
 	@Autowired
