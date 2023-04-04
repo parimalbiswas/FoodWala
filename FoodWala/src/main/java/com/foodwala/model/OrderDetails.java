6def9ccffd1cf82f03b8041d9688068095b327b3
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-04-04 18:08:39 +0530

    .

--- FoodWala/src/main/java/com/foodwala/model/OrderDetails.java
+++ FoodWala/src/main/java/com/foodwala/model/OrderDetails.java
@@ -1,9 +1,6 @@
 package com.foodwala.model;
 
 import java.time.LocalDateTime;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,18 +17,18 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrderDetails {
+public class OrderDetails
+{
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@OneToOne
 	private Cart cart;
-	
+
 	private String status;
-	
+
 	private LocalDateTime time;
-	
-	
-	
+
 }
