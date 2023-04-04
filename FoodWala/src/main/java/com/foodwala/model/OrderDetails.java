c7dfd021111b7e8f5e7396ce0011fa6fb29ea753
pawanyadav96 <zib494@gmail.com>
2023-04-04 18:05:24 +0530

    ??????????????????ppp

--- FoodWala/src/main/java/com/foodwala/model/OrderDetails.java
+++ FoodWala/src/main/java/com/foodwala/model/OrderDetails.java
@@ -1,26 +1,37 @@
 package com.foodwala.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-public class Bill {
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer billId;
-
-	private Integer totalItem;
-	
-	private Integer totalCost;
+	private Integer id;
 	
 	@OneToOne
-	private OrderDetails order;
+	private Cart cart;
+	
+	private String status;
 	
 	private LocalDateTime time;
+	
+	
+	
 }
